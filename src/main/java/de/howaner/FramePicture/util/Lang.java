81fbfc340c09c5736af62b9bb8b4062c1ebc49ea
Howaner <franzi.moos@googlemail.com>
2014-08-15 23:54:16 +0200

    Changed default language.

--- src/main/java/de/howaner/FramePicture/util/Lang.java
+++ src/main/java/de/howaner/FramePicture/util/Lang.java
@@ -11,26 +11,26 @@ import de.howaner.FramePicture.FramePicturePlugin;
 public enum Lang {
 	PLUGIN_ENABLED("Plugin enabled!"),
 	PLUGIN_DISABLED("Plugin disabled!"),
-	NO_PLAYER("&4You are not a Player!"),
-	NO_PERMISSION("&4No Permission!"),
-	ALREADY_SELECTION("&4You have already a Selection!"),
-	CLICK_FRAME("&3Please click on the Frame!"),
+	NO_PLAYER("&4You are not a player!"),
+	NO_PERMISSION("&4No permission!"),
+	ALREADY_SELECTION("&4You have already a selection!"),
+	CLICK_FRAME("&3Please click on the frame!"),
 	CLICK_MULTIFRAME("&3Please click on the top left Item Frame!"),
-	GETTING_MODE_ENABLED("&2Getting Mode enabled!"),
-	GETTING_MODE_DISABLED("&2Getting Mode disabled!"),
+	GETTING_MODE_ENABLED("&2Getting-Mode enabled!"),
+	GETTING_MODE_DISABLED("&2Getting-Mode disabled!"),
 	CREATING_CANCELLED("&3Creating cancelled!"),
 	PLUGIN_RELOAD("&2Plugin reloaded!"),
 	FRAME_SET("&2Frame with ID &7#%id&2 created!"),
 	MULTIFRAME_SET("&2%amount frames created!"),
-	NO_FRAMEPICTURE("&4This is not a Frame Picture!"),
-	GET_URL("&3Infos from Frame:\n&fID: &7#%id\n&fURL: &7%url"),
+	NO_FRAMEPICTURE("&4This isn't a frame picture!"),
+	GET_URL("&3Frame infos:\n&fID: &7#%id\n&fURL: &7%url"),
 	NO_PICTURE("&4Unknown picture format: %url"),
 	PREFIX("&7[&6FramePicture&7] &f"),
 	NOT_ENOUGH_MONEY("&4You haven't enough Money!"),
-	PLEASE_WAIT("&7Please wait.."),
+	PLEASE_WAIT("&7Please wait ..."),
 	FRAME_REMOVED("&3Frame removed!"),
-	ALREADY_FRAME("&4This is already a Frame Picture!"),
-	DOWNLOAD_ERROR("&4An error occurred while downloading the Picture!");
+	ALREADY_FRAME("&4This is already a frame picture!"),
+	DOWNLOAD_ERROR("&4An error occurred while downloading the picture!");
 	
 	private final String value;
 	public static YamlConfiguration config = null;
